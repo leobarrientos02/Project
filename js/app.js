955c591b66080a3b9aec526c192b27c7d5fcaab5
Your Name <leobarrientos02@gmail.com>
2021-09-10 06:25:04 -0400

    major changes

--- js/app.js
+++ js/app.js
@@ -1,20 +1,32 @@
-/* Selectors
-const profile = document.querySelector(".profile-image");
+const category_container = document.querySelector(".categories");
+const category1 = document.querySelector(".category1");
+const category2 = document.querySelector(".category2");
+const category3 = document.querySelector(".category3");
+const category4 = document.querySelector(".category4");
+const category5 = document.querySelector(".category5");
+const category6 = document.querySelector(".category6");
+const category7 = document.querySelector(".category7");
+const category8 = document.querySelector(".category8");
+const category9 = document.querySelector(".category9");
+const category10 = document.querySelector(".category10");
+const category11 = document.querySelector(".category11");
+const category12 = document.querySelector(".category12");
+const arrow_back = document.querySelector(".arrow-back");
+const arrow_f = document.querySelector(".arrow-forward");
 
-//Event Listeners
-profile.addEventListener("click", () => {
-  document.querySelector(".dropdown-content").classList.toggle("show");
-});
+arrow_back.addEventListener("click", (event) => {
+  category1.style.display = "none";
+  category2.style.display = "none";
+  category3.style.display = "none";
+  category4.style.display = "none";
+  category5.style.display = "none";
+  category6.style.display = "none";
+  arrow_back.style.display = "none";
 
-// Show Password function
-const icon = document.querySelector(".password-icon");
-var x = document.getElementById("password");
-icon.addEventListener("click", () => {
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
+  category12.style.display = "block";
+  category7.style.display = "block";
+  category11.style.display = "block";
+  category10.style.display = "block";
+  category9.style.display = "block";
+  category8.style.display = "block";
 });
-// Show password function ends */
-const auth = "Ps9lQtHCDzcpbhj37P5PFDroF7kaJx";
