6c80420901d1f2509247b8c8e80656b207236d2e
Your Name <leobarrientos02@gmail.com>
2021-09-10 22:40:28 -0400

    little things

--- js/user.js
+++
@@ -1,9 +0,0 @@
-const user = {
-    userid = "1",
-    firstName = "John",
-    lastName = "Doe",
-    userName = "Admin",
-    email = "leobarrientos02@gmail.com",
-    user = "Admin",
-    password = "password"
-}
