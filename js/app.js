1d8053fcf3e7d25e47526b18ee3e00ade654c9d4
Your Name <leobarrientos02@gmail.com>
2021-09-12 21:47:32 -0400

    trying something different

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const ArrowDown1 = document.getElementById("clients");
+const ArrowDown2 = document.getElementById("worker");
+const ArrowDown3 = document.getElementById("why");
+
+ArrowDown1.addEventListener("mouseover", () => {
+  console.log("leo");
+});
