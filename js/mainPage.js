e862a0f5813eb18b5749625994a88a9233e6fab5
Your Name <leobarrientos02@gmail.com>
2021-09-13 15:14:52 -0400

    coded another card

--- js/mainPage.js
+++ js/mainPage.js
@@ -1,75 +1,4 @@
-const topSkills1 = [
-  "Data Entry Specialists",
-  "Video Editors",
-  "Data Analysts",
-  "Shopify Developer",
-  "Ruby on Rials Developer",
-  "Android Developer",
-  "Bookkeeper",
-  "Content Writer",
-  "Copywriter",
-  "Database Administrator",
-  "Data Scientist",
-  "Front-End Developer",
-  "Game Developer",
-  "Graphic Developer",
-  "iOS Developer",
-  "Java Developer",
-];
-const topSkills2 = [
-  "JavaScript Developer",
-  "Logo Designer",
-  "Mobile App Developer",
-  "PHP Developer",
-  "Python Developer",
-  "Resume Writer",
-  "SEO Expert",
-  "Social Media Manager",
-  "Software Developer",
-  "Software Engineer",
-  "Technical Writer",
-  "UI Designer",
-  "UX Designer",
-  "Virtual Assistant",
-  "Web Designer",
-  "Wordpress Developer",
-];
-const TrendingSkills1 = [
-  "Blockchain",
-  "Go development",
-  "Node.js",
-  "Vue.js",
-  "HR consulting",
-  "Microsoft Power BI",
-  "Instructional design",
-  "React.js",
-  "Videographers",
-  "HTML5 Developers",
-  "Ghostwriters",
-  "Unity 3D Developers",
-  "Business Consultants",
-  "Coders",
-  "Marketing Consultants",
-  "Web Developers",
-];
-const TrendingSkills2 = [
-  "Illustrators",
-  "Google AdWords Experts",
-  "Digital Marketers",
-  "Project Managers",
-  "Arduino Programmers",
-  "Ruby Developers",
-  "AngularJS Devleopers",
-  "Full Stack Developers",
-  "Email Marketing Consultants",
-  "React Native Developers",
-  "Swift Developers",
-  "CSS Developers",
-  "Google Sketchup Freelancers",
-  "Back End Developers",
-  "Smartsheet Freelancers",
-  "Zoom Video Conferencing Freelancers",
-];
+//SKills Directory Functionality Getting Elements
 const topSkillBtn = document.getElementById("topSkill");
 const trendingSkillBtn = document.getElementById("trendingSkills");
 const UsTopSkillBtn = document.getElementById("topSkillsUs");
@@ -165,8 +94,9 @@ projectsBtn.addEventListener("click", () => {
   projectsListDiv1.classList.add("show");
   projectsListDiv2.classList.add("show");
 });
+// Skills Directory Tree ends here
 
-//Header arrows
+// Header arrows Functionality
 const ArrowDown1 = document.getElementById("clients");
 const ArrowDown2 = document.getElementById("worker");
 const ArrowDown3 = document.getElementById("why");
@@ -174,3 +104,4 @@ const ArrowDown3 = document.getElementById("why");
 ArrowDown1.addEventListener("mouseover", () => {
   console.log("leo");
 });
+// Header arrows Functionality Ends Here
