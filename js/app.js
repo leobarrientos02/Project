f3ec3e6d18d1e713225dbe780b42a50fa60e49f5
Your Name <leobarrientos02@gmail.com>
2021-09-15 02:24:06 -0400

    skills section completed

--- js/app.js
+++
@@ -1,7 +0,0 @@
-const ArrowDown1 = document.getElementById("clients");
-const ArrowDown2 = document.getElementById("worker");
-const ArrowDown3 = document.getElementById("why");
-
-ArrowDown1.addEventListener("mouseover", () => {
-  console.log("leo");
-});
