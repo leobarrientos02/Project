530813698ada785754ddd14489c577d75baf4b84
Your Name <leobarrientos02@gmail.com>
2021-09-27 16:12:13 -0400

    adding show and hide password

--- login/loginPage.js
+++ login/loginPage.js
@@ -1,16 +1,17 @@
 //Show Password functionality
-const checkbox = document.getElementById("showPassword");
-const show = document.getElementById("show");
+const showPswrd = document.getElementById("show");
 
-checkbox.addEventListener("click", showPassword);
+showPswrd.addEventListener("click", showPassword);
 
 function showPassword() {
   var x = document.getElementById("password");
-  if (x.type === "password") {
+  if (x.value === "") {
+  } else if (x.type === "password") {
     x.type = "text";
     show.innerText = "Hide Password";
   } else {
     x.type = "password";
+
     show.innerText = "Show Password";
   }
 }
