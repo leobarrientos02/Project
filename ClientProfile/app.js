8cf37482671628d1dbaa716c0845a2ea50086816
Your Name <leobarrientos02@gmail.com>
2021-10-03 16:17:57 -0400

    logic error

--- ClientProfile/app.js
+++ ClientProfile/app.js
@@ -8,3 +8,19 @@ profileImage.addEventListener("click", () => {
 sessionCard.addEventListener("click", () => {
   sessionCard.classList.remove("isVisible");
 });
+
+const mobileSearchCard = document.querySelector(".mobileSearchCard");
+const searchIcon = document.getElementById("mobileSearch");
+searchIcon.addEventListener("click", () => {
+  mobileSearchCard.style.display = "inline-block";
+  searchIcon.style.display = "none";
+  navIcon.style.opacity = "0";
+  exitSearch.style.display = "inline-block";
+});
+const exitSearch = document.querySelector(".fa-times");
+exitSearch.addEventListener("click", () => {
+  mobileSearchCard.style.display = "none";
+  searchIcon.style.display = "inline-block";
+  navIcon.style.opacity = "1";
+  exitSearch.style.display = "none";
+});
