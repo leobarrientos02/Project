6703c57d99878a5a14bf1a750a0e6c2e130e51b7
Your Name <leobarrientos02@gmail.com>
2021-10-05 03:00:06 -0400

    PostAJob Complete now starting get job detail

--- FunctionPages/PostAJob.js
+++ FunctionPages/PostAJob.js
@@ -3,6 +3,9 @@ const option2 = document.getElementById("option2");
 
 const option1Circle = document.getElementById("option1Circle");
 const option2Circle = document.getElementById("option2Circle");
+const continueBtn = document.getElementById("continue");
+
+var choosen = 0;
 
 option1.addEventListener("click", () => {
   option1.style.border = "1.5px solid #054e97";
@@ -12,6 +15,11 @@ option1.addEventListener("click", () => {
   option1Circle.style.border = ".7px solid #054e97";
   option2Circle.style.color = "transparent";
   option2Circle.style.border = ".7px solid lightgrey";
+
+  continueBtn.style.background = "#054e97";
+  continueBtn.style.color = "white";
+  continueBtn.style.cursor = "pointer";
+  choosen = 1;
 });
 option2.addEventListener("click", () => {
   option1.style.border = "1.25px solid lightgrey";
@@ -21,4 +29,21 @@ option2.addEventListener("click", () => {
   option1Circle.style.border = ".7px solid lightgrey";
   option2Circle.style.color = " #054e97";
   option2Circle.style.border = ".7px solid #054e97";
+
+  continueBtn.style.background = "#054e97";
+  continueBtn.style.color = "white";
+  continueBtn.style.cursor = "pointer";
+  choosen = 2;
+});
+
+continueBtn.addEventListener("click", () => {
+  if (choosen === 1) {
+    localStorage.setItem("TimePeriod", "Short Term");
+    window.location.href = "./GetJobDetail.html";
+  } else if (choosen === 2) {
+    localStorage.setItem("TimePeriod", "Long Term");
+    window.location.href = "./GetJobDetail.html";
+  } else {
+    localStorage.setItem("TimePeriod", null);
+  }
 });
