20f1ae3d8861a936604d6c6adaec90c23b2753e1
Your Name <leobarrientos02@gmail.com>
2021-10-13 14:27:18 -0400

    postJobpage skeleton done

--- PostAJob/reviewJob.js
+++ PostAJob/reviewJob.js
@@ -6,6 +6,36 @@ const reviewJobTitle = (document.getElementById("currentTitleSpan").innerText =
 const confirmTitle = document.getElementById("confirmTitle");
 confirmTitle.addEventListener("click", () => {
   var ctSpan = document.getElementById("currentTitleSpan");
-  console.log(ctSpan.innerText);
+  //console.log(ctSpan.innerText);
   localStorage.setItem("Job_Title", ctSpan.innerText);
 });
+
+const jobDescription = document.getElementById("jobDescription");
+
+const currentCategory = localStorage.getItem("category");
+const selectedCategory = (document.getElementById(
+  "selectedCategory"
+).innerText = currentCategory);
+
+const currentScopeSize = localStorage.getItem("projectSize");
+const currentScopeTime = localStorage.getItem("TimeScope");
+const currentScopeExperience = localStorage.getItem("Experience");
+
+const selectedScope = (document.getElementById("selectedScope").innerText =
+  currentScopeSize
+    .concat(", ")
+    .concat(currentScopeTime)
+    .concat(", ")
+    .concat(currentScopeExperience));
+
+const currentBudget = localStorage.getItem("budget");
+const selectedBudget = document.getElementById("selectedBudget");
+selectedBudget.innerText = currentBudget;
+
+const currentLocation = localStorage.getItem("locations");
+const selectedLocation = document.getElementById("selectedLocation");
+selectedLocation.innerText = currentLocation;
+
+const currentSkills = localStorage.getItem("skills");
+const selectedSkills = document.getElementById("selectedSkills");
+selectedSkills.innerText = currentSkills;
