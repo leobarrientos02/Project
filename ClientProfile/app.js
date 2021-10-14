d963e8a1e8de44b9454a7afe4794f6ba0ab9cc67
Your Name <leobarrientos02@gmail.com>
2021-10-14 05:44:05 -0400

    adding a godMode

--- ClientProfile/app.js
+++ ClientProfile/app.js
@@ -69,3 +69,22 @@ if (localStorage.getItem("JobCount") == 0) {
   result.style.display = "none";
   postedJob.style.display = "inline-block";
 }
+
+const jobGodMode = document.getElementById("jobGodMode");
+const jobEdit = document.querySelector(".jobEdit");
+jobGodMode.addEventListener("click", () => {
+  jobEdit.style.display = "flex";
+});
+
+const exitJobEdit = document.getElementById("exitJobEdit");
+exitJobEdit.addEventListener("click", () => {
+  jobEdit.style.display = "none";
+});
+
+const deleteJob = document.getElementById("deleteJob");
+deleteJob.addEventListener("click", () => {
+  jobEdit.style.display = "none";
+  localStorage.setItem("JobCount", 0);
+  postedJob.style.display = "none";
+  result.style.display = "flex";
+});
